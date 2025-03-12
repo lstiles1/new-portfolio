38f3218e26a7a191ff1fcedbc01182e7f6213216
lstiles1 <75706206+lstiles1@users.noreply.github.com>
2025-03-12 11:32:29 -0700

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import { Header } from "@/sections/Header";
+import { HeroSection } from "@/sections/Hero";
+import { ProjectsSection } from "@/sections/Projects";
+import { AboutSection } from "@/sections/About";
+import { ContactSection } from "@/sections/Contact";
+import { Footer } from "@/sections/Footer";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <HeroSection />
+      <ProjectsSection  />
+      <AboutSection />
+      <ContactSection />
+      <Footer />
+    </div>
+  );
+}
