43bc541cc50427fd6351f3d15f5ee6c15b2d663c
lstiles1 <75706206+lstiles1@users.noreply.github.com>
2025-06-18 11:50:53 -0700

    new resume/grammar

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -74,7 +74,7 @@ export const HeroSection = () => {
         <div className='max-w-lg mx-auto'>
           <h1 className='font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide'>Liam Stiles</h1>
           <p className='mt-4 text-center text-white/60 md:text-lg'> 
-          I am full-stack developer based in San Francisco with a passion for building functional, high-performing web applications.
+          I am a full-stack developer based in San Francisco with a passion for building functional, high-performing web applications.
           </p>
         </div>
         <div className='flex flex-col md:flex-row justify-center items-center mt-8 gap-4 relative z-50'>
